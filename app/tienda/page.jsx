112fcaa9b1ad94ffedfd9392889c767dc2010f50
Sonia Pacheco <103387638+soniapachecomza@users.noreply.github.com>
2024-04-11 15:59:28 -0300

    inicio de rutas

---
+++ app/tienda/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Tienda(){
+    return (
+        <div>
+            <h1>Tienda</h1>
+
+            <ul>
+                <li>Producto 1</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Tienda;
